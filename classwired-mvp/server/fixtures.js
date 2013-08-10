5282e066163b895a4cfdf85e951d0a724932b821
Lindsay Rattray <lindsayrattray@yahoo.com>
2013-08-10 11:31:18 +1000

    Added Inputs Collection

---
+++ classwired-mvp/server/fixtures.js
@@ -0,0 +1,14 @@
+if (Inputs.find().count() === 0) {
+	Inputs.insert({
+		content: 'chips',
+		author: 'Ahmed',
+	});
+	Inputs.insert({
+		content: 'apple',
+		author: 'Noriko',
+	});
+	Inputs.insert({
+		content: 'tomato',
+		author: 'Ahmed',
+	});
+}
