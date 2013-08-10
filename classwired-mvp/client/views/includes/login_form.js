4676088df43a919efec5dc5992b374350abf4765
Fiachna <fiachna.carter@gmail.com>
2013-08-10 16:26:51 +1000

    store user id in session

--- classwired-mvp/client/views/includes/login_form.js
+++ classwired-mvp/client/views/includes/login_form.js
@@ -1,13 +1,14 @@
 Template.loginForm.events({
 	'submit': function() {
 		var username = $('#username-input').val();
-		Users.insert({
+		var userId = Users.insert({
 			name: username
 		});
+
 		Session.set('username', username);
+		Session.set('userId', userId);
 		Session.set('alert', username);
-		alert(username);
-		alert(Session.get('username'));
+		
 		return false;
 	}
 });
