48e6492b7f4579316f080d3f785be2d5f257a218
Fiachna <fiachna.carter@gmail.com>
2013-08-11 16:29:59 +1000

    fix neglected rebase conflict in activity_form.js

--- classwired-mvp/client/views/includes/activity_form.js
+++ classwired-mvp/client/views/includes/activity_form.js
@@ -3,30 +3,19 @@ Template.activityForm.events({
 		var myInput = $('#myInput');
 		var myInputValue = myInput.val();
 		var username = Session.get('username');
-<<<<<<< HEAD
-		
-=======
 		var roomId = Session.get('room-id');
-
->>>>>>> allow switching of room state
+		
 		if(username != undefined)
 		{
 			Inputs.insert({
 				content: myInputValue,
-<<<<<<< HEAD
-				author: username
-			});
-			Session.set('new-input-available', 'true');
-		}
-
-		myInput.val('');
-
-=======
 				author: username,
 				room: roomId
 			});
 		}
->>>>>>> allow switching of room state
+
+		myInput.val('');
+
 		return false;
 	}
 });
