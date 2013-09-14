aea4bbc5282fcf069f0a2f368b0113848c23c715
Fiachna <fiachna.carter@gmail.com>
2013-09-14 20:28:39 +1000

    partial implementation of login flow

--- classwired-mvp/client/views/includes/login.js
+++ classwired-mvp/client/views/includes/login.js
@@ -11,24 +11,50 @@ function debugLogin(error)
 }
 
 Template.login.events({
-	'click #signup-btn': function() 
-	{
-		var options = {};
-
-		options.username = $('#username-field').val();
-		options.password = $('#password-field').val();
-
-		Accounts.createUser(options, function(error) { debugLogin(error); });
-	},
-	'click #login-btn': function()
-	{
-		var username = {username: $('#username-field').val()};
-		var password = $('#password-field').val();
-
-		Meteor.loginWithPassword(username, password, function(error, result) { debugLogin(error); });
-	},
 	'click #logout-btn': function()
 	{
 		Meteor.logout(function(error) { debugLogin(error); });
+	},
+	'submit': function(event, template)
+	{
+		var options = {};
+		var emailAddress = template.find('#email-field').value;
+		var name = template.find('#name-field').value;
+		var password = template.find('#password-field').value;
+		var user = Meteor.users.findOne({emails: { $elemMatch: { address: emailAddress } } });
+
+		if(user)
+		{
+			if(!password)
+			{
+				$('#login-email-container').addClass('hidden');
+				$('#login-password-container').removeClass('hidden');
+			}
+			else
+			{
+				console.log(emailAddress + ' ' + password);
+				//log in
+			}
+		}
+		else
+		{
+			if(!name && !password)
+			{
+				$('#login-email-container').addClass('hidden');
+				$('#login-name-container').removeClass('hidden');
+			}
+			else if(name && !password)
+			{
+				$('#login-name-container').addClass('hidden');
+				$('#login-password-container').removeClass('hidden');
+			}
+			else
+			{
+				console.log(emailAddress + ' ' + name + ' ' + password);
+				//sign up
+			}
+		}
+
+		event.preventDefault();
 	}
 });
