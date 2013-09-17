2c8ad6529cc0ffebbd5af0e28bfac379e82ea3a8
Fiachna <fiachna.carter@gmail.com>
2013-09-17 10:40:26 +1000

    restructure app

--- classwired-mvp/server/lib/methods/user_controller.js
+++ classwired-mvp/server/lib/methods/user_controller.js
@@ -1,13 +1,3 @@
-// Create admin role if none exists
-Meteor.startup(function() {
-	var admin = Meteor.users.findOne({username: 'administrator'});
-	if(!admin)
-	{
-		admin = Accounts.createUser({ email: 'admin@classwired.com', username: 'administrator', password: 'cwAdministrator', profile: { name: "Administrator" } });
-		Meteor.users.update(admin, { $set: { permissions: [UserController.availablePermissions()[0], UserController.availablePermissions()[1], UserController.availablePermissions()[2]] } });
-	}
-});
-
 Meteor.methods({
 	addUserToRole: function(userId, rolename) {
 		if(UserController.roleExists(rolename) && !UserController.userHasRole(userId, rolename))
