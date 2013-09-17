ee003d2510c3ae6e9430907f76227a7684b1465a
Fiachna <fiachna.carter@gmail.com>
2013-09-17 13:39:59 +1000

    prevent multiple entries against room participants

--- classwired-mvp/server/lib/methods/classroom_controller.js
+++ classwired-mvp/server/lib/methods/classroom_controller.js
@@ -1,16 +1,12 @@
 Meteor.methods({
 	createClassroom: function(name, userId) {
-		if(Classrooms.findOne({ name: name }))
-		{
-
-		}
-		else
+		if(!Classrooms.findOne({ name: name }))
 		{
 			Classrooms.insert({name: name, owner: userId, currentActivity: 'classroom', participants: [], open: true});
 		}
 	},
 	addParticipantToClassroom: function(classroomId, userId) {
-		if(Classrooms.findOne(classroomId) && Meteor.users.findOne(userId))
+		if(Classrooms.findOne(classroomId) && Meteor.users.findOne(userId) && Classrooms.findOne(classroomId).participants.indexOf(userId) == -1)
 		{
 			Classrooms.update(classroomId, { $push: { participants: userId } });
 		}
