5daa803178658f2b9b9d7d4edd4bbf527fbf1a49
Fiachna <fiachna.carter@gmail.com>
2013-09-19 14:49:53 +1000

    draft group handler methods

--- classwired-mvp/server/lib/methods/group_handler.js
+++ classwired-mvp/server/lib/methods/group_handler.js
@@ -2,7 +2,46 @@ Meteor.methods({
 	createGroup: function(classroomId, userId) {
 		Groups.insert({ classroomId: classroomId, members: [userId] });
 	},
-	mergeGroups: function() {
+	addUserToGroup: function(groupId, userToAddEmailAddress) {
+		var userToAdd = Meteor.users.find({ "emails.address": userToAddEmailAddress });
+		if(userToAdd)
+		{
+			Groups.update(groupId, { $push: { members: userToAdd._id } });
+		}
+	},
+	removeUserFromGroup: function(groupId, userId) {
+		Groups.update(groupId, { $pull: { members: userId } });
+	},
+	mergeGroups: function(fromGroupId, toGroupId) {
+		var fromMembers = Groups.findOne(fromGroupId).members;
+		var toMembers = Groups.findOne(toGroupId).members;
 
+		if(fromMembers > toMembers)
+		{
+			for(member in toMembers)
+			{
+				Groups.update(fromGroupId, { $push: { members: member } });
+			}
+			Groups.remove(toGroupId);
+		}
+		else
+		{
+			for(member in fromMembers)
+			{
+				Groups.update(toGroupId, { $push: { members: member } });
+			}
+			Groups.remove(fromGroupId);
+		}
+	},
+	disbandGroup: function(groupId) {
+		var groupToDisband = Groups.findOne(groupId);
+		var members = groupToDisband.members;
+
+		for(member in members)
+		{
+			Groups.insert({ classroomId: groupToDisband.classroomId, members: [member] });
+		}
+
+		Groups.remove(groupId);
 	}
 });
