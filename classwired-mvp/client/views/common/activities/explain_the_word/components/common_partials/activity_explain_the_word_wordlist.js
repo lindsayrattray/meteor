234359a25b3ca4a3eb31bde9f417aa97b2e2b4be
Fiachna <fiachna.carter@gmail.com>
2013-09-20 15:38:55 +1000

    allow users to enter words into wordlist

--- classwired-mvp/client/views/common/activities/explain_the_word/components/common_partials/activity_explain_the_word_wordlist.js
+++ classwired-mvp/client/views/common/activities/explain_the_word/components/common_partials/activity_explain_the_word_wordlist.js
@@ -1,6 +1,11 @@
+var updateWordlist = _.debounce(function(item, content) {
+	ExplainTheWord_WordlistItems.update(item._id, { $set: { item: content } });
+	Session.set('editingItem', false);
+}, 1000, false);
+
 Template.activityExplainTheWord_Wordlist_Wordlist.rendered = function() {
-	var focusedItems = $('*:focus').length
-	if(focusedItems === 0)
+	var isEditing = Session.get('editingItem')
+	if(!isEditing)
 	{
 		var $wordlistContainer = $('#wordlist-container');
 		$wordlistContainer.animate({ scrollTop: $wordlistContainer.prop("scrollHeight") }, 500);
@@ -8,7 +13,12 @@ Template.activityExplainTheWord_Wordlist_Wordlist.rendered = function() {
 };
 
 Template.activityExplainTheWord_Wordlist_Wordlist.events({
-	
+	'input .txt-wordlist-item-input': function(event, template) {
+		updateWordlist(this, event.srcElement.value);
+	},
+	'focus .txt-wordlist-item-input': function() {
+		Session.set('editingItem', true);
+	}
 });
 
 Template.activityExplainTheWord_Wordlist_Wordlist.helpers({
