3ffb6a81bd2e959000f212eccc4e00b7cd2d9220
Fiachna <fiachna.carter@gmail.com>
2013-09-20 14:14:29 +1000

    scroll wordlist on new item created when nothing is focused

--- classwired-mvp/client/views/common/activities/explain_the_word/components/common_partials/activity_explain_the_word_wordlist.js
+++ classwired-mvp/client/views/common/activities/explain_the_word/components/common_partials/activity_explain_the_word_wordlist.js
@@ -1,6 +1,15 @@
 Template.activityExplainTheWord_Wordlist_Wordlist.rendered = function() {
-	console.log('wordlist updated');
+	var focusedItems = $('*:focus').length
+	if(focusedItems === 0)
+	{
+		var $wordlistContainer = $('#wordlist-container');
+		$wordlistContainer.animate({ scrollTop: $wordlistContainer.prop("scrollHeight") }, 500);
+	}
 };
+
+Template.activityExplainTheWord_Wordlist_Wordlist.events({
+	
+});
 
 Template.activityExplainTheWord_Wordlist_Wordlist.helpers({
 	wordlistItems: function() {
--- classwired-mvp/client/views/common/activities/explain_the_word/components/common_partials/activity_explain_the_word_wordlist_entry.js
+++ classwired-mvp/client/views/common/activities/explain_the_word/components/common_partials/activity_explain_the_word_wordlist_entry.js
@@ -4,7 +4,16 @@ Template.activityExplainTheWord_Wordlist_Entry.events({
 		if(user)
 		{
 			var group = Groups.findOne({ members: user._id, classroomId: template.data.classroom._id });
-			ExplainTheWord_WordlistItems.insert({ item: '', userId: user._id, groupId: group._id, classroomId: template.data.classroom._id });
+			var wordlistItem =  {
+									item: '',
+									userId: user._id,
+									groupId: group._id,
+									classroomId: template.data.classroom._id
+								};
+			if(!ExplainTheWord_WordlistItems.findOne(wordlistItem))
+			{
+				ExplainTheWord_WordlistItems.insert(wordlistItem);	
+			}
 		}
 	}
 });
