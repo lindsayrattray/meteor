90c4620c5ad1c23b7cd6a67e05c1f777b1251278
Fiachna <fiachna.carter@gmail.com>
2013-09-23 11:43:03 +1000

    create methods for assigning items

--- classwired-mvp/server/lib/activity_methods/explain_the_word_explain_methods.js
+++ classwired-mvp/server/lib/activity_methods/explain_the_word_explain_methods.js
@@ -1,11 +1,44 @@
+var ensureUniqueItem = function(wordlistItems, explainItems)
+{
+	var result = _.sample(wordlistItems);
+	explainItems = _.reject(explainItems, {item: result.item});
+	if(explainItems.length > 0)
+	{
+		result = ensureUniqueItem(wordlistItems, explainItems);
+	}
+	return result;
+}
+
 Meteor.methods({
 	assignNewItem: function(groupId) {
+		var wordlistItems = ExplainTheWord_WordlistItems.find().fetch();
+		var explainItems = ExplainTheWord_ExplainItems.find().fetch();
+		var currentItem = ExplainTheWord_ExplainItems.findOne({ groupId: groupId, current: true });
+		var newItem = ensureUniqueItem(wordlistItems, explainItems);
+		var timestamp = new Date();
 
+		if(currentItem)
+		{
+			ExplainTheWord_ExplainItems.update(currentItem._id, { $set: { current: false } });
+		}
+		ExplainTheWord_ExplainItems.insert({ item: newItem.item, classroomId: newItem.classroomId, groupId: groupId, current: true, assigned_timestamp: timestamp, answered: false, answer: '' });
 	},
 	assignSpecificItem: function(groupId, itemId) {
+		var currentItem = ExplainTheWord_ExplainItems.findOne({ groupId: groupId, current: true });
+		var newItem = ExplainTheWord_WordlistItems.findOne(itemId);
+		var timestamp = new Date();
 
+		if(currentItem)
+		{
+			ExplainTheWord_ExplainItems.update(currentItem._id, { $set: { current: false } });
+		}
+		ExplainTheWord_ExplainItems.insert({ item: newItem.item, classroomId: newItem.classroomId, groupId: groupId, current: true, assigned_timestamp: timestamp, answered: false, answer: '' });
 	},
-	assignNewItemToAllGroups: function() {
-
+	assignNewItemToAllGroups: function(classroomId) {
+		var groups = Groups.find({ classroomId: classroomId }).fetch();
+		for(group in groups)
+		{
+			Meteor.call('assignNewItem', groups[group]._id);
+		}
 	}
 });
