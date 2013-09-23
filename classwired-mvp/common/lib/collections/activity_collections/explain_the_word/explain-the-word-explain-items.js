1bf8e76b34a1a623dfb6ae55e4fdc9ddb30b3333
Fiachna <fiachna.carter@gmail.com>
2013-09-23 11:03:08 +1000

    create explain item collection and publication

---
+++ classwired-mvp/common/lib/collections/activity_collections/explain_the_word/explain-the-word-explain-items.js
@@ -0,0 +1,1 @@
+ExplainTheWord_ExplainItems = new Meteor.Collection('explainTheWord_ExplainItems');
---
+++ classwired-mvp/server/lib/activity_methods/explain_the_word_explain_methods.js
@@ -0,0 +1,11 @@
+Meteor.methods({
+	assignNewItem: function(groupId) {
+
+	},
+	assignSpecificItem: function(groupId, itemId) {
+
+	},
+	assignNewItemToAllGroups: function() {
+
+	}
+});
