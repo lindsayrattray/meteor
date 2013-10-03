8460b8b0e33d01d946258c5b21b0d8581aa477af
Fiachna <fiachna.carter@gmail.com>
2013-10-03 11:28:51 +1000

    cleanup code

--- classwired-mvp/client/views/common/classrooms/classroom.js
+++ classwired-mvp/client/views/common/classrooms/classroom.js
@@ -25,25 +25,6 @@ Template.classroom.events({
 		Meteor.call('setUserCurrentRoom', Meteor.user()._id, null);
 		Router.go('/');
 	},
-	/*'click .btn-join-group': function(event, template) {
-		var user = Meteor.user();
-		var classroom = template.data._id;
-		var targetId = this._id;
-		var currentGroup = Groups.findOne({ classroomId: classroom, members: user._id });
-
-		if(targetId !== currentGroup._id)
-		{
-			Meteor.call('mergeGroups', currentGroup._id, targetId);
-		}
-
-		currentGroup = Groups.findOne({ classroomId: classroom, members: user._id });
-	},
-	'click .btn-remove-user-from-group': function(event, template) {
-		var user = Meteor.users.findOne(event.srcElement.dataset.member);
-		var classroom = template.data._id;
-		var group = Groups.findOne({ classroomId: classroom, members: user._id });
-		Meteor.call('removeUserFromGroup', group._id, user._id);
-	},*/
 	'click .component-selector': function(event, template) {
 		var classroom = template.data._id;
 		var component = this.name;
@@ -69,8 +50,5 @@ Template.classroom.helpers({
 	},
 	components: function() {
 		return Components.find();
-	},
-	/*groups: function() {
-		return Groups.find();	
-	}*/
+	}
 });
