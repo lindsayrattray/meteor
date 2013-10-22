bb07fec406e6ff28268c55934c2259b95c748457
Fiachna <fiachna.carter@gmail.com>
2013-10-22 16:48:34 +1100

    change methods to work using groupId

--- classwired-mvp/server/lib/activity_methods/explain_the_word_explain_methods.js
+++ classwired-mvp/server/lib/activity_methods/explain_the_word_explain_methods.js
@@ -21,13 +21,13 @@ Meteor.methods({
 	populateItems: function(groupId, classroomId, userId) {
 		var explainItems = _.pluck(ExplainTheWord_ExplainItems.find({ classroomId: classroomId, groupId: groupId }).fetch(), 'item');
 		var wordlistItems = _.chain(ExplainTheWord_WordlistItems.find({ classroomId: classroomId }).fetch()).pluck('item').uniq().difference(explainItems).value();
-		var currentItem = ExplainTheWord_ExplainItems.findOne({ assignedTo: userId.toString(), classroomId: classroomId });
+		var currentItem = ExplainTheWord_ExplainItems.findOne({ assigned_to: userId.toString(), classroomId: classroomId });
 
 		if(wordlistItems.length > 0)
 		{
 			for(itemIndex in wordlistItems)
 			{
-				ExplainTheWord_ExplainItems.insert({ item: wordlistItems[itemIndex], classroomId: classroomId, groupId: groupId, assignedTo: null, assigned_timestamp: null, answered: false, answer: null});
+				ExplainTheWord_ExplainItems.insert({ item: wordlistItems[itemIndex], classroomId: classroomId, groupId: groupId, assigned_to: null, assigned_timestamp: null, answered: false, answer: null});
 			}
 			if(!currentItem)
 			{
@@ -36,38 +36,29 @@ Meteor.methods({
 		}
 	},
 	assignNewItem: function(userId, classroomId) {
+		var group = GroupManager.getGroupByMember(userId, classroomId);
+		var groupId = group ? group._id : null;
 		var wordlistItems = ExplainTheWord_WordlistItems.find({ classroomId: classroomId }).fetch();
-		var explainItems = ExplainTheWord_ExplainItems.find({ userId: userId.toString(), answered: true, classroomId: classroomId }).fetch();
-		var currentItem = ExplainTheWord_ExplainItems.findOne({ assignedTo: userId.toString(), current: true, classroomId: classroomId });
+		var explainItems = ExplainTheWord_ExplainItems.find({ groupId: groupId, answered: true, classroomId: classroomId }).fetch();
+		var currentItem = ExplainTheWord_ExplainItems.findOne({ groupId: groupId, assigned_to: userId.toString(), classroomId: classroomId });
 		var newItem = ensureUniqueItem(wordlistItems, explainItems, currentItem);
 		var timestamp = new Date();
 
 		if(currentItem)
 		{
-			ExplainTheWord_ExplainItems.update(currentItem._id, { $set: { current: false } });
+			ExplainTheWord_ExplainItems.update(currentItem._id, { $set: { assigned_to: userId } });
 		}
 		if(newItem)
 		{
-			var oldItem = ExplainTheWord_ExplainItems.findOne({ item: newItem.item, classroomId: newItem.classroomId, userId: userId, answered: false });
+			var oldItem = ExplainTheWord_ExplainItems.findOne({ item: newItem.item, classroomId: newItem.classroomId, groupId: groupId, answered: false });
 			if(oldItem)
 			{
 				timestamp = oldItem.assigned_timestamp ? oldItem.assigned_timestamp : timestamp;
-				ExplainTheWord_ExplainItems.update(oldItem._id, { $set: { current: true, assigned_timestamp: timestamp } });
+				ExplainTheWord_ExplainItems.update(oldItem._id, { $set: { assigned_to: userId, assigned_timestamp: timestamp } });
 			}
 			else
 			{
-				ExplainTheWord_ExplainItems.insert({ item: newItem.item, classroomId: newItem.classroomId, userId: userId, current: true, assigned_timestamp: timestamp, answered: false, answer: null });
-			}
-		}
-	},
-	assignNewItemToAllUsers: function(classroomId) {
-		var groups = Groups.find({ classroomId: classroomId }).fetch();
-		for(groupIndex in groups)
-		{
-			var members = groups[groupIndex].members;
-			for(memberIndex in members)
-			{
-				Meteor.call('assignNewItem', members[memberIndex], classroomId);
+				ExplainTheWord_ExplainItems.insert({ item: newItem.item, classroomId: newItem.classroomId, groupId: groupId, assigned_to: userId, assigned_timestamp: timestamp, answered: false, answer: null });
 			}
 		}
 	}
