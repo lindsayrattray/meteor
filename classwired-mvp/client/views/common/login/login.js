e72b8633c65be37177ab2fde40c79821cbd263e0
Fiachna <fiachna.carter@gmail.com>
2013-10-23 15:29:05 +1100

    clean up code

--- classwired-mvp/client/views/common/login/login.js
+++ classwired-mvp/client/views/common/login/login.js
@@ -108,33 +108,4 @@ Template.login.events({
 
 		event.preventDefault();
 	}
-	/*'submit .login form': function(event, template)
-	{
-		var loginState = Session.get('loginState');
-		var nameField = template.find('.name input');
-		var emailField = template.find('.email input');
-		var passwordField = template.find('.password input');
-
-		var emailAddress = emailField.value;
-		var name = nameField.value;
-		var password = passwordField.value;
-
-		event.preventDefault();
-
-		if(loginState === 'signup')
-		{
-			Accounts.createUser({   email: emailAddress,
-									password: password,
-									profile: { name: name } },
-									function(error) { 
-														var userId = Meteor.users.findOne({ "emails.address": emailAddress });
-														Meteor.call('addUserToRole', userId, 'student');
-														debugLogin(error);
-													});
-		}
-		else
-		{
-			Meteor.loginWithPassword({ email: emailAddress }, password, function(error) { debugLogin(error) });
-		}
-	}*/
 });
