b5961d4df6eddd7b6818f226b434683dc9ae4644
Fiachna <fiachna.carter@gmail.com>
2013-10-30 11:31:01 +1100

    add submit buttons

--- classwired-mvp/client/views/common/splash/splash.js
+++ classwired-mvp/client/views/common/splash/splash.js
@@ -1,5 +1,20 @@
+var userName, email, loginState, loginStage;
+
+Template.splash.events({
+
+});
+
 Template.splash.helpers({
 	loggingIn: function() {
 		return Meteor.loggingIn();
+	},
+	studentName: function() {
+		return userName;
+	},
+	debug: function() {
+		console.log(userName);
+		console.log(email);
+		console.log(loginState);
+		console.log(loginStage);
 	}
-})
+});
