87672465f4dcdc83d52d98c162e2a57b42cbd44e
Fiachna <fiachna.carter@gmail.com>
2013-11-20 12:56:56 +1100

    add server side methods to handle create and delete classroom

--- classwired-mvp/server/lib/methods/classroom_manager_methods.js
+++ classwired-mvp/server/lib/methods/classroom_manager_methods.js
@@ -1,10 +1,18 @@
 Meteor.methods({
-	createClassroom: function(name, userId, description) {
+	createClassroom: function(name, user, description) {
 		if(!Classrooms.findOne({ name: name }))
 		{
-			Classrooms.insert({ name: name, owner: userId, description: description, currentActivity: null, currentActivityComponent: '', open: true, date_created: Date.parse(new Date) });
+			Classrooms.insert({ name: name, owner: user._id, description: description, currentActivity: null, currentActivityComponent: '', open: true, date_created: Date.parse(new Date) });
 		}
 	},
+	deleteClassroom: function(classroom, user)
+	{
+		var target = Classrooms.findOne(classroom);
+		if(target)
+		{
+			Classrooms.remove(target._id);
+		}
+	}
 	setCurrentActivity: function(classroomId, activityId) {
 		var classroom = Classrooms.findOne(classroomId);
 		if(classroom && classroom.currentActivity !== activityId && Activities.findOne(activityId))
