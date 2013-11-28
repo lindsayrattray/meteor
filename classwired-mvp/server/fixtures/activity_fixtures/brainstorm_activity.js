e098319fee1a7f866bafe2747b63f53ff9104930
Fiachna <fiachna.carter@gmail.com>
2013-11-29 10:34:28 +1100

    add description and short description to activity fixtures

--- classwired-mvp/server/fixtures/activity_fixtures/brainstorm_activity.js
+++ classwired-mvp/server/fixtures/activity_fixtures/brainstorm_activity.js
@@ -3,7 +3,7 @@ var brainstormActivityId = brainstormActivity ? brainstormActivity._id : null;
 
 if(!brainstormActivityId)
 {
-	brainstormActivityId = Activities.insert({ name: 'brainstorm', title: 'Brainstorm', template: 'activityBrainstorm_Main'});
+	brainstormActivityId = Activities.insert({ name: 'brainstorm', title: 'Brainstorm', shortDescription: 'A simple brainstorming activity', description: 'This is a longer description that can be filled out later. \n lets test a newline \n or two.', template: 'activityBrainstorm_Main'});
 }
 
 var brainstormComponentsObjects = [{ name: 'brainstorm', title: 'Brainstorm', template: 'activityBrainstorm_Brainstorm', activityId: brainstormActivityId }];
