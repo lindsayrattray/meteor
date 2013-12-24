cc10eb2fb4c2e1a5bf56e796fd9484bd4a09a620
Fiachna <fiachna.carter@gmail.com>
2013-12-24 16:38:45 +1100

    assign permissions to generated users

--- classwired-mvp/server/fixtures/default_users.js
+++ classwired-mvp/server/fixtures/default_users.js
@@ -1,10 +1,15 @@
 var userCount = Meteor.users.find().count();
-if(userCount === 1 || userCount === 0)
+if(userCount <= 1)
 {
   teacher = Accounts.createUser({ email: 'teacher@domain.com', username: 'teacher', password: 'test', profile: { name: "Teacher" } });
   Meteor.users.update(teacher, {$set: {permissions: 'teacher'}});
 
-  Accounts.createUser({ email: 'student1@domain.com', username: 'student1', password: 'test', profile: { name: "Student 1" } });
-  Accounts.createUser({ email: 'student2@domain.com', username: 'student2', password: 'test', profile: { name: "Student 2" } });
-  Accounts.createUser({ email: 'student3@domain.com', username: 'student3', password: 'test', profile: { name: "Student 3" } });
+  var thisUserId = Accounts.createUser({ email: 'student1@domain.com', username: 'student1', password: 'test', profile: { name: "Student 1" } });
+  Meteor.users.update(thisUserId, { $set: { permissions: 'student' } });
+
+  thisUserId = Accounts.createUser({ email: 'student2@domain.com', username: 'student2', password: 'test', profile: { name: "Student 2" } });
+  Meteor.users.update(thisUserId, { $set: { permissions: 'student' } });
+
+  thisUserId = Accounts.createUser({ email: 'student3@domain.com', username: 'student3', password: 'test', profile: { name: "Student 3" } });
+  Meteor.users.update(thisUserId, { $set: { permissions: 'student' } });
 }
